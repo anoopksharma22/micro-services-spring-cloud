c845631aa59e5b938461c1bf54cd059fc3cb9b4b
Anoop Sharma <anoopksharma22@gmail.com>
2025-01-10 16:09:49 +0530

    gateway added

---
+++ gateway-ms/src/test/java/in/itsanoop/gateway_ms/GatewayMsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.itsanoop.gateway_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
